11bb1d4a67bd40637b1278bf9cd411c28c120ba1
windowsworldcartoon <windowsworldcartoon@gmail.com>
2025-06-13 21:10:56 -0500

    feat: Remove pg dependency
    This commit removes the `pg` dependency from the project. The `pg` dependency was previously used for database interactions, but it is no longer needed.
    
    The following changes were made:
    
    - Removed the `pg` dependency from the `package.json` file.
    - Removed the `Pool` import from the `server.js` file.
    - Removed all related `pg` packages.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
-const { Pool } = require('pg');
 const expressSession = require('express-session');
 
 
