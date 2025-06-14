1df01e47088aecf46f5b16bde3dbb6da47fce230
windowsworldcartoon <windowsworldcartoon@gmail.com>
2025-06-13 19:27:31 -0500

    update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const geocoder = require('geocoder');
 const axios = require('axios');
 const app = express();
 const port = 3000;
 
+
+
+app.get('/', (req, res) => {
+  res.send([]);
+});
 
 
 app.get('/weather/:city', (req, res) => {
